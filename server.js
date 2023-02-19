43eb653ee5071d11749cac9b472d3728c405536a
ethan clark <esclark99@gmail.com>
2023-02-19 13:01:34 -0500

    test0

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
