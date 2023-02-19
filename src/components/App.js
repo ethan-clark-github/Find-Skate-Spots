51386ae000119f93bcbbf3c5299f0cf0d2af388d
ethan clark <esclark99@gmail.com>
2023-02-19 10:29:44 -0500

    fixed local host port for heroku

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ function App() {
     // })
 
     useEffect(() => {
-        fetch('http://localhost:4005/skatespots')
+        fetch('http://localhost:3000/skatespots')
             .then(res => res.json())
             .then((data) => setSkatespots(data));
     }, [])
@@ -33,7 +33,7 @@ function App() {
 
 
     function handleAddSkatespot(newSkatespot) {
-        fetch("http://localhost:4005/skatespots", {
+        fetch("http://localhost:3000/skatespots", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
