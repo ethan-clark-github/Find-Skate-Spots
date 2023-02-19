715fcde3597f0b22916865cba8ae3a04cc7fb1a4
ethan clark <esclark99@gmail.com>
2023-02-19 14:17:13 -0500

    test17

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ function App() {
     // })
 
     useEffect(() => {
-        fetch('https://find-skate-spots.herokuapp.com:3000/api')
+        fetch('https://find-skate-spots.herokuapp.com/skatespots')
             .then(res => res.json())
             .then((data) => setSkatespots(data));
     }, [])
@@ -33,7 +33,7 @@ function App() {
 
 
     function handleAddSkatespot(newSkatespot) {
-        fetch("https://find-skate-spots.herokuapp.com:3000/api", {
+        fetch("https://find-skate-spots.herokuapp.com/skatespots", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
